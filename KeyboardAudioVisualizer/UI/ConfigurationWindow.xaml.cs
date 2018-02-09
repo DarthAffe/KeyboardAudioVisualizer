940d2eb631e7a33472adef3c2e220caebb999efb
Darth Affe <darthaffe@wyrez.org>
2018-02-09 21:12:33 +0100

    Added a workaround for the weird windows taskbar-closewindow behavior

--- KeyboardAudioVisualizer/UI/ConfigurationWindow.xaml.cs
+++ KeyboardAudioVisualizer/UI/ConfigurationWindow.xaml.cs
@@ -1,9 +1,16 @@
-﻿using KeyboardAudioVisualizer.Controls;
+﻿using System;
+using KeyboardAudioVisualizer.Controls;
 
 namespace KeyboardAudioVisualizer.UI
 {
     public partial class ConfigurationWindow : BlurredDecorationWindow
     {
         public ConfigurationWindow() => InitializeComponent();
+
+        //DarthAffe 07.02.2018: This prevents the applicaiton from not shutting down and crashing afterwards if 'close' is selected in the taskbar-context-menu
+        private void ConfigurationWindow_OnClosed(object sender, EventArgs e)
+        {
+            ApplicationManager.Instance.ExitCommand.Execute(null);
+        }
     }
 }
